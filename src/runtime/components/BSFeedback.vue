2d6f0a3b8dfe6d2dccc19ce474215e75b4e4250a
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-02-12 19:59:04 +0100

    Adding BSFeedback component.

---
+++ src/runtime/components/BSFeedback.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    const { valid } = defineProps<{
+        valid: boolean
+    }>();
+</script>
+
+<template>
+    <div :class="{'valid-feedback': valid, 'invalid-feedback': !valid }">
+        <slot />
+    </div>
+</template>
