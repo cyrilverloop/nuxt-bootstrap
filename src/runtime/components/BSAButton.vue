fec2c9ef44a12bc2296e5ff6a5eb4890e4786dcd
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-02-12 19:56:03 +0100

    Adding BSButton and BSAButton components.

---
+++ src/runtime/components/BSAButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+    import { BSVariant } from "#nbs";
+
+    const {
+        to,
+        variant = BSVariant.primary,
+        outline = false,
+        textNoWrap = false
+    } = defineProps<{
+        to : string|object,
+        variant?: BSVariant|"link",
+        outline?: boolean,
+        textNoWrap?: boolean
+    }>();
+
+    const fullBSVariant = computed(() => {
+
+        if(outline === true) {
+            return "btn-outline-" + variant;
+        }
+
+        return "btn-" + variant;
+    });
+</script>
+
+<template>
+    <NuxtLinkLocale
+        :to="to"
+        :class="[fullBSVariant, {'text-nowrap': textNoWrap}]"
+        role="button" class="btn">
+        <slot />
+    </NuxtLinkLocale>
+</template>
---
+++ src/runtime/components/BSButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+    import { BSVariant } from "#nbs";
+
+    const {
+        variant = BSVariant.primary,
+        outline = false,
+        textNoWrap = false
+    } = defineProps<{
+        variant?: BSVariant|"link",
+        outline?: boolean,
+        textNoWrap?: boolean
+    }>();
+
+    const fullBSVariant = computed(() => {
+
+        if(outline === true) {
+            return "btn-outline-" + variant;
+        }
+
+        return "btn-" + variant;
+    });
+</script>
+
+<template>
+    <button
+        type="button"
+        class="btn"
+        :class="[fullBSVariant, {'text-nowrap': textNoWrap}]"
+    >
+        <slot />
+    </button>
+</template>
