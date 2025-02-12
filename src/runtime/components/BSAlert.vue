ba3599434cf146b2a3fe9dbe674d03f057d160f7
Cyril VERLOOP <cyril.verloop@webqard.fr>
2025-02-12 19:54:07 +0100

    Adding BSAlert component.

---
+++ src/runtime/components/BSAlert.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+    import { BSVariant } from "#nbs";
+
+    const {
+        variant = BSVariant.primary,
+        dismissible = false
+    } = defineProps<{
+        variant?: BSVariant,
+        dismissible?: boolean
+    }>();
+</script>
+
+<template>
+    <div class="alert" role="alert"
+        :class="[`alert-${variant}`, {'alert-dismissible': dismissible}]">
+
+        <slot />
+
+        <button
+            v-if="dismissible"
+            type="button" class="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close">
+        </button>
+    </div>
+</template>
